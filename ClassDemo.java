1346873192a2dc62cc7924a727e0fb7ef5d0bee1
sharmi-ravi <144410211+sharmi-ravi@users.noreply.github.com>
2024-08-21 21:26:19 +0530

    Add files via upload

---
+++ ClassDemo.java
@@ -0,0 +1,11 @@
+class Box{
+int length,breadth,height;
+}
+public class ClassDemo{
+public static void main(String[] args){
+Box blackbox=new Box();
+blackbox.length=10;
+blackbox.height=12;
+blackbox.breadth=13;
+}
+}
